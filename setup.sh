7107711fc0ebd493577a489235e16e55ba328f02
joseph pea <jpea@imvu.com>
2017-03-13 17:32:05 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,6 +15,17 @@
     # if Arch Linux
     #sudo pacman -S git
 
+# Python
+    brew install python
+    #pacman -S python
+
+# Node.js
+    brew install nodejs
+    #sudo pacman -S nodejs
+
+# GNU Coreutils (prefixed with g in OSX, already present in Linux)
+    brew install coreutils
+
 # Clone dotfiles
 # TODO: consolidate into single repo.
     mkdir -p ~/src
@@ -30,14 +41,6 @@
     ln -s ~/src/trusktr+dotfiles1/.editorconfig
     ln -s ~/src/trusktr+dotfiles1/.jshintrc
 
-# Python
-    brew install python
-    #pacman -S python
-
-# Node.js
-    brew install nodejs
-    #sudo pacman -S nodejs
-
 # Vim/Neovim
     cd ~
     ln -s ~/src/trusktr+dotfiles2/.vimrc/.vimrc
@@ -46,6 +49,7 @@
     mkdir -p ~/.config/nvim
     ln -s ~/.vimrc ~/.config/nvim/init.vim
 
+    brew install libtool gettext
     brew install neovim/neovim/neovim
     #pacman -S neovim
 
@@ -54,18 +58,17 @@
     sudo npm install -g jshint # use by neomake plugin.
 
 # ZSH
+    cd ~/src
     git clone git@github.com:zsh-users/antigen.git zsh-users+antigen
+    cd ~
     ln -s ~/src/trusktr+dotfiles2/.zshrc
     brew install zsh
     #sudo pacman -S zsh
 
     # Run once:
-    # TODO: detect if ran already.
+    # TODO: detect if ran already. For now, check manually and enable if needed.
     command -v zsh | sudo tee -a /etc/shells
     sudo chsh -s "$(command -v zsh)" "${USER}"
-
-# GNU Coreutils (prefixed with g in OSX, already present in Linux)
-    brew install coreutils
 
 # Chrome
 
